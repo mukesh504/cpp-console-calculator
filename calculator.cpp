13b036a07d732c24db69ae21a37cd0af83921494
Mukesh <mukeshtech1605@gmail.com>
2026-08-21 11:59:47 +0530

    add parser input navigation helpers

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,41 @@
-#include<iostream>
-#include<string>
-#include<cctype>
-#include<stdexcept>
-#include<cmath>
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <stdexcept>
+#include <cmath>
 
-int main(){
-    std::cout<<" C++ Console Calculator\n";
+class Parser{
+    private:
+         const std::string& src;
+        size_t pos;
+    
+    public:
+         Parser(const std::string& text) : src(text),pos(0){
+
+         }
+         double parseNumber(){
+            size_t start = pos;
+
+            while (pos<src.size()&&std::isdigit(static_cast<unsigned char>(src[pos])) || src[pos] == '.')
+            {
+                pos++;
+            }
+            return std::stod(src.substr(start,pos-start));
+            
+         };
+
+}
+
+int main() {
+    std::cout << "C++ Console Calculator\n";
+
+    std::string expression;
+
+    std::cout << "Enter Expression: ";
+    std::getline(std::cin, expression);
+
+    std::cout << "You entered: " << expression << '\n';
+
+
     return 0;
 }
